199030845ae44f9ab79a57833a29d2979d8c8b75
Joe Spaeth <43396274+jss5ha@users.noreply.github.com>
2021-12-08 21:04:59 -0500

    Update app.py

--- flaskstuff/app.py
+++ flaskstuff/app.py
@@ -5,8 +5,8 @@ import sys
 from sklearn.feature_extraction.text import CountVectorizer
 
 app = Flask(__name__)
-model = pickle.load(open('logregmodel.sav', 'rb'))
-vectorizer = pickle.load(open("vectorizer.sav", "rb"))
+model = pickle.load(open('DecisionTree.sav', 'rb'))
+vectorizer = pickle.load(open("newvectorizer.sav", "rb"))
 
 @app.route('/')
 def home():
@@ -22,9 +22,27 @@ def predict():
     int_features = vectorizer.transform(int_features) #should only be the one value
     #final_features = [np.array(int_features)] #dont need this one
     prediction = model.predict(int_features)
-
+    prob = model.predict_proba(int_features)
+    prob = prob.tolist()
+    print("pred type is ", type(prediction))
+    print(prediction)
+    print("prob type is ", type(prob))
+    print(prob)
+    prediction = int (prediction[0])
+    print("pred type is now ", type(prediction))
+    if prediction == 1:
+        print("here")
+        prediction = "normal"
+        prob = prob[0][1]
+    else:
+        print("here")
+        prediction = "fraud"
+        prob=prob[0][0]
+    #i scp'ed this !!!
     #return render_template('index.html', prediction_text='This phone call is $ {}'.format(prediction))
-    return ('This phone call is $ {}'.format(prediction))
+    print(prob)
+    return ('This phone call is ' + prediction + " with probability " + str(prob))
 
 if __name__ == "__main__":
+    #this is changed to run at localhost 8080 for AWS
     app.run(debug=True)
